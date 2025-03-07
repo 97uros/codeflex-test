c6b20e8c1127aa3771b5cdc4a1d24fd54d97e37d
Uroš Ostojić <urosostojic97@gmail.com>
2025-03-07 10:21:46 +0100

    Enqueue parent theme styles

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+// Child theme styles
+
+function twentytwentyfour_child_enqueue_styles() {
+  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'twentytwentyfour_child_enqueue_styles');
